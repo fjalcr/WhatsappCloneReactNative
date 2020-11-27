a35a541d06ca524949ddb6c7229c52d9458d575d
Javier Alcocer <falcocer@xcaret.com>
2020-11-26 19:25:55 -0500

    users screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
@@ -8,12 +8,57 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 import { withAuthenticator } from 'aws-amplify-react-native';
-import Amplify from 'aws-amplify';
+import Amplify, { API, Auth, graphqlOperation } from 'aws-amplify';
 import config from './aws-exports';
+
+import { getUser } from './graphql/queries';
+import { createUser } from './graphql/mutations';
+import { ClippingRectangle } from 'react-native';
 Amplify.configure(config);
 
 
 function App() {
+
+  //run only on first render
+  useEffect(() => {
+    const fetchUsers = async () => {
+      //get auth user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true})
+      if(userInfo){
+        //get user from backend with user id from
+        const userData = await API.graphql(
+            graphqlOperation(
+              getUser,
+              {id:userInfo.attributes.sub}
+              )
+          )
+
+          
+          if(userData.data.getUser){
+            console.log('user is registered')
+          }else{
+            
+            const newUser = {
+              id: userInfo.attributes.sub,
+              name: userInfo.username,
+              imageUri: `https://ui-avatars.com/api/?name=${userInfo.username}`,
+              status:'Hey there i am using Whatsapp!'
+            }
+
+            await API.graphql(
+              graphqlOperation(
+                createUser,
+                {input: newUser}
+              )
+            )
+          }
+
+        }
+  
+    }
+    fetchUsers()
+  },[])
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,10 +1,31 @@
-import * as React from 'react';
+import React, { useEffect,useState } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { View } from '../components/Themed';
 import  ContactListItem  from '../components/ContactListItem ';
 
-import users from '../data/Users';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../graphql/queries';
+
+
+
 export default function ChatScreen() {
+  const [ users, setUsers ] = useState({})
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try { 
+        const usersList = await API.graphql(
+          graphqlOperation(
+            listUsers
+          )
+        )
+        setUsers(usersList.data.listUsers.items)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUsers()
+  },[])
+
   return (
     <View style={styles.container}>
       <FlatList
